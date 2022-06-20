7b20cb9d55076e0b0c699fab76f79134d6846401
jaydenpersonnat <89941534+jaydenpersonnat@users.noreply.github.com>
2022-06-20 19:57:46 -0400

    first commit

---
+++ Problem Sets/mario.py
@@ -0,0 +1,21 @@
+                                # Mario 
+
+"""
+Create a program that prints the following shape with a user-inputted height: 
+
+~/workspace/ $ mario.py
+height: 5
+    ##
+   ###
+  ####
+ #####
+######
+
+
+Specification: 
+    1. First prompt the user for the height of the pyramid 
+    2. Generate the desired half-pyramid (with the help of print and one or more loops)
+    3. Take care to align the bottom-left corner of your half-pyramid with the left-hand edge of your terminal window.
+"""
+
+# Code goes below here 
